3c3cbff0a773a2ae9e287cd67e91b9b50e4622c4
JERONIFAITHJ <jeronij10@gmail.com>
2022-10-31 23:02:43 +0530

    Cleared import bug

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import './App.css';
 import LandingPage from './Components/Pages/LandingPage';
 import { Redirect, Switch } from 'react-router-dom';
-import { Route } from '@mui/icons-material';
+import { Route } from 'react-router-dom';
+import Pages from './Components/Pages/Pages';
+import Navbar from './Components/Navbar/Navbar';
+import Shop from './Components/Pages/Shop';
+import Contact from './Components/Pages/Contact';
+import About from './Components/Pages/About';
 
 function App() {
   return (
     <div className="App">
-      {/* <Switch>
+      <Navbar />
+      <Switch>
         <Route path='/' exact>
             <Redirect to='/home'></Redirect>
         </Route>
         <Route path='/home' exact>
           <LandingPage />
         </Route>
-      </Switch> */}
-      <LandingPage />
+        <Route path='/pages' exact>
+          <Pages />
+        </Route>
+        <Route path='/blog' exact>
+          <Pages />
+        </Route>
+        <Route path='/shop' exact>
+          <Shop />
+        </Route>
+        <Route path='/contact' exact>
+          <Contact />
+        </Route>
+        <Route path='/about' exact>
+          <About />
+        </Route>
+      </Switch>
     </div>
   );
 }
